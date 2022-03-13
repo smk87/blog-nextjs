4324a9e6f3851c2c7928e1cb5ab7a811d35cd4bf
Saqlain Mahmud <sqlnmhmdkhanemon@gmail.com>
2022-03-13 14:25:19 +0600

    feat: Initialize nextJs project with Typescript.

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+const HomePage: NextPage = () => {
+    return (
+        <main>
+            <h1>My Blog</h1>
+        </main>
+    );
+};
+
+export default HomePage;
